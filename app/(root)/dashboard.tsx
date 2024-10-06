1286d58819651b2e6413f54727259f5acbc7b34b
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-10-06 22:32:20 +0530

    UI changes in dashboard

--- app/(root)/dashboard.tsx
+++ app/(root)/dashboard.tsx
@@ -5,23 +5,38 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useState } from 'react';
 
 const students = [
-  { name: 'Abhinav Mishra', class: 11, level: 'Excellent', color: 'bg-green-100', progressColor: 'bg-green-500' },
-  { name: 'Roger Lubin', class: 12, level: 'Optimal', color: 'bg-yellow-100', progressColor: 'bg-yellow-500' },
-  { name: 'Charlie Rhiel Madsen', class: 12, level: 'Inefficient', color: 'bg-red-100', progressColor: 'bg-red-500' },
-  { name: 'Chance Stanton', class: 11, level: 'Excellent', color: 'bg-green-100', progressColor: 'bg-green-500' },
-  { name: 'Chance Bay', class: 11, level: 'Excellent', color: 'bg-green-100', progressColor: 'bg-green-500' },
-  { name: 'Chance Jhon', class: 11, level: 'Excellent', color: 'bg-green-100', progressColor: 'bg-green-500' },
+  { name: 'Abhinav Mishra', class: 11, level: 'Excellent', color: 'bg-green-100' },
+  { name: 'Roger Lubin', class: 12, level: 'Optimal', color: 'bg-yellow-100' },
+  { name: 'Charlie Rhiel Madsen', class: 12, level: 'Inefficient', color: 'bg-red-100' },
+  { name: 'Chance Stanton', class: 11, level: 'Excellent', color: 'bg-green-100' },
+  { name: 'Chance Bay', class: 11, level: 'Excellent', color: 'bg-green-100' },
+  { name: 'Chance Jhon', class: 11, level: 'Excellent', color: 'bg-green-100' },
 ];
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState('All');
+  const getLevelPercentage = (level:any) => {
+    switch(level) {
+      case 'Inefficient':
+        return { percentage: 30, color: 'bg-red-500' };
+      case 'Optimal':
+        return { percentage: 65, color: 'bg-yellow-500' };
+      case 'Excellent':
+        return { percentage: 100, color: 'bg-green-500' };
+      default:
+        return { percentage: 0, color: 'bg-gray-300' };
+    }
+  };
   return (
     <SafeAreaView className="bg-[#FEFBFF] flex-1">
       {/* Header */}
       <View className="px-4 pt-0 pb-[16px] flex-row justify-between items-center">
         <Text className="text-[26px] font-bold">Dashboard</Text>
         <View className="flex-row gap-[16px] items-center">
-          <FontAwesome name="bell" size={24} color="black" className="mr-4" />
+          <View className='bg-[#832CFF14] py-[7px] px-[9px] rounded-[7.58px]'>
+          <FontAwesome name="bell-o" size={20} color="black" className='' />
+          </View>
+       
           <Image
             source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }}
             className="w-8 h-8 rounded-full"
@@ -60,33 +75,41 @@ const Dashboard = () => {
         <Text className="text-[16px] font-medium text-[#9654F4] mb-2">See All</Text>
         </View>
         
-        <FlatList
-          data={students}
-          keyExtractor={(item) => item.name}
-          renderItem={({ item }) => (
-            <View className={`p-4 mb-4 rounded-lg ${item.color}`}>
-              <View className="flex-row justify-between items-center">
-                <View className="flex-row items-center">
-                  <Image
-                    source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }}
-                    className="w-12 h-12 rounded-full mr-4"
-                  />
-                  <View>
-                    <Text className="text-lg font-bold">{item.name}</Text>
-                    <Text className="text-gray-500">Class: {item.class}</Text>
-                  </View>
-                </View>
-                <View className="items-center">
-                  <Text>Level:</Text>
-                  <View className="w-24 h-2 rounded-full bg-gray-300">
-                    <View className={`h-2 rounded-full ${item.progressColor}`} />
+          <FlatList
+            data={students}
+            keyExtractor={(item) => item.name}
+           renderItem={({ item }) => {
+          const { percentage, color } = getLevelPercentage(item.level);
+
+            return (
+              <View className={`p-4 mb-4 rounded-lg ${item.color}`}>
+                <View className="flex-row justify-between items-center">
+                  <View className="flex-row items-center">
+                    <Image
+                      source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }}
+                      className="w-12 h-12 rounded-full mr-4"
+                    />
+                    <View className='items-start flex-col'>
+                      <Text className="text-lg font-bold">{item.name}</Text>
+                      <Text className="text-gray-500">Class: {item.class}</Text>
+                      <View className="items-center flex-row gap-[10px]">
+                      <Text>Level:</Text>
+                      <View className="w-[50%] h-2 rounded-full bg-gray-300">
+                        <View
+                          className={`h-2  rounded-full ${color}`}
+                          style={{ width: `${percentage}%` }}
+                        />
+                      </View>
+                      </View>
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
-          )}
-          showsVerticalScrollIndicator={false}
-        />
+              );
+            }}
+            contentContainerStyle={{ paddingBottom: 16 }}
+            showsVerticalScrollIndicator={false}
+          />
       </View>
     </SafeAreaView>
   );
