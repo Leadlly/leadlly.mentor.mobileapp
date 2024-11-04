4a7f11a5e1a9c102f49cbef84e6fd2d2c8fff035
shivang-16 <shivangyadav121@gmail.com>
2024-11-04 22:43:52 +0530

    fix: caching

--- services/queries/trackerQuery.ts
+++ services/queries/trackerQuery.ts
@@ -1,30 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
-import axiosClient from '../axios/axios';// Ensure this points to your axios instance
+import axiosClient from '../axios/axios'; 
 
-export const getTracker = async (subject: string | string[], id: any) => {
+export const getTracker = (subject: string | string[], id: any) => {
     return useQuery({
-        queryKey: ['TrackerData'],
+        queryKey: ['TrackerData', id, subject], 
         queryFn: async () => {
             try {
-      
-              // API call using axiosClient
-              const res: AxiosResponse = await axiosClient.get(
-                `/api/student/tracker/get/${id}?subject=${subject}`
-              );
-      
-              const responseData = res.data;
-              console.log(res._response.data,"this si res")
-              return responseData;
+                // API call using axiosClient
+                const res: AxiosResponse = await axiosClient.get(
+                    `/api/student/tracker/get/${id}?subject=${subject}`
+                );
+
+                return res.data;
             } catch (error) {
-              console.error('Error fetching tracker data');
-              if (axios.isAxiosError(error)) {
-                throw new Error(`${error.response?.data.message}`);
-              } else {
-                throw new Error('An unknown error occurred while fetching tracker data');
-              }
+                console.error('Error fetching tracker data');
+                if (axios.isAxiosError(error)) {
+                    throw new Error(error.response?.data.message || 'An error occurred');
+                } else {
+                    throw new Error('An unknown error occurred while fetching tracker data');
+                }
             }
-          },
-    })
-  };
-  
+        },
+        staleTime: 0,
+        refetchOnWindowFocus: true, 
+    });
+};
